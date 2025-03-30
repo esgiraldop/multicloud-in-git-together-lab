078f6cddf12393737c93119a65674ff47273e112
Erick Giraldo <santiagogiraldo_514@hotmail.com>
2025-03-30 00:20:48 +0000

    Small change to return string

--- Lambda/HelloGitTogetherLambda/index.js
+++ Lambda/HelloGitTogetherLambda/index.js
@@ -4,6 +4,6 @@ exports.handler = async (event, context) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
     console.log('Community Info: ', event.message);
     console.log('Console for test #2');
-    return "Holaa Gente Frojjjm Git Together";
+    return "Hola Gente From Git Together. This is a deployment from Erick Giraldo";
     // throw new Error('Something went wrong');
 };
